e56a8cf832f8bd5808e33c2f47142012dfe2cb8d
Muskan Kumari <muskangen1395@gmail.com>
2025-08-21 13:34:49 +0530

    #

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -14,10 +14,10 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
     const companion = await getCompanion(id);
     const user = await currentUser();
 
-    const { name, subject, title, topic, duration } = companion;
-
     if(!user) redirect('/sign-in');
-    if(!name) redirect('/companions')
+    if(!companion) redirect('/companions');
+
+    const { name, subject, title, topic, duration } = companion;
 
     return (
         <main>
